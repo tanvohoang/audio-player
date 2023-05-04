f5f8f1d3fdeec96e3b447e084fe20bfc8a627d62
unknown <vohtan43@gmail.com>
2023-05-05 00:21:22 +0700

    push everything

---
+++ src/Player/audios.js
@@ -0,0 +1,14 @@
+exports.songsdata = [
+    {
+        "title": "Your Betrayal - Bullet for My Valentine",
+        "url": "https://www.nhaccuatui.com/mh/auto/sWTSjCIKMiey"
+    },
+    {
+        "title": "Linkin Park - In the end",
+        "url": "https://www.nhaccuatui.com/mh/auto/7UquwkolCh"
+    },
+    {
+        "title": "Seize The Day - Avenged Sevenfold",
+        "url": "https://www.nhaccuatui.com/mh/auto/dez4HSdemR3W"
+    }
+]
